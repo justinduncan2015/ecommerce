fdf6845be723140c5951f4f64071bf17beaa834d
BrianLewis7 <b.lewis@knights.ucf.edu>
2015-11-15 14:40:42 -0500

    Updated version
    Hopefully this fixes the issue of it not working.

--- checkout3.php
+++ checkout3.php
@@ -1,23 +1,36 @@
+<!DOCTYPE html>
 <?php
-$pageTitle = 'Batchpad.com - Checkout';
-include_once('header.php');
+ include_once('header.php');
+ include_once('db_connect.php');
+ include_once('cart_update.php');
+ $cart_itm = $_SESSION["cart_products"];
 ?>
-<div id="categorymenu">
-	<nav class="subnav">
-		<ul class="nav-pills categorymenu container">
-			<li><a class="active home" href="home.html"><i class="icon-home icon-white font18"></i> <span> Home</span></a></li>
-			<li><a href="catalog.html">Shop</a></li>
-			<li><a href="about.html">about</a></li>
-			<li><a href="contact.html">Contact Us</a> </li>
-			<li class="pull-right">
-    		<form action="search.php" method="post" class="form-search top-search">
-        		<input type="text" name="search" class="input-small search-query" placeholder="Search Here…">
-        		<button class="btn btn-orange btn-small tooltip-test" data-original-title="Search"><i class="icon-search icon-white"></i></button>
-    		</form>
-			</li>
-		</ul>
-	</nav>
-</div>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>BatchPad Checkout - Justin Duncan</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet" type="text/css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/prettyPhoto.css" rel="stylesheet" media="screen">
+<link href="css/portfolio.css" rel="stylesheet">
+<link rel="stylesheet" href="layerslider/css/layerslider.css" type="text/css">
+<link rel="stylesheet" href="layerslider/css/layersliderstyle.css" type="text/css">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+<!-- fav -->
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+</head>
+<body>
+<!-- Header Start -->
+
 <div id="maincontainer">
   <section id="product">
     <div class="container">
@@ -37,65 +50,118 @@ include_once('header.php');
         <!-- Account Login-->
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
         <h2 class="heading1"><span class="maintext">Checkout</span></h2>
-        <div class="checkoutsteptitle">Personal information</div>
+        <div class="checkoutsteptitle">Payment  Method</div>
         <div class="checkoutstep">
-          <p>Please ensure these details are correct before continuing.</p>
-          
+          <p>Please select the preferred payment method to use on this order.</p>
+          <div class="radio-inline">
+          <label class=" inline">
+            <input type="radio" value="option1">
+            Credit Card</label>
+            <label class=" inline">
+            <input type="radio" value="option1">
+            Paypal</label>
+            <label class=" inline">
+            <input type="radio" value="option1">
+            Android Pay</label>
+            </div>
           <br>
         </div>
         <div class="checkoutsteptitle">Confirm Order </div>
         <div class="checkoutstep">
-          <div class="cart-info">
-            <table class="table table-striped table-bordered">
-              <tr>
-                <th class="image">Image</th>
-                <th class="name">Product Name</th>
-                <th class="model">Model</th>
-                <th class="quantity">Quantity</th>
-                <th class="price">Unit Price</th>
-                <th class="total">Total</th>
-              </tr>
-              <tr>
-                <td class="image"><a ><img title="product" alt="product" src="img/prodcut-40x40.jpg" height="50" width="50"></a></td>
-                <td   class="name"><a >product1</a></td>
-                <td class="model">Product1</td>
-                <td class="quantity"><input type="text" size="1" value="1" name="quantity[40]" class="span1">
-                  &nbsp; <a  class="mr10"> <i class="tooltip-test font24 icon-refresh " data-original-title="Update"> </i> </a> <a ><i class="tooltip-test font24 icon-remove-circle" data-original-title="Remove"> </i></a></td>
-                <td class="price">$130.00</td>
-                <td class="total">$110.25</td>
-              </tr>
-              <tr>
-                <td class="image"><a ><img title="product" alt="product" src="img/prodcut-40x40.jpg" height="50" width="50"></a></td>
-                <td   class="name"><a >Product 1</a></td>
-                <td class="model">product 1</td>
-                <td class="quantity"><input type="text" size="1" value="1" name="quantity[40]" class="span1">
-                  &nbsp; <a  class="mr10"> <i class="tooltip-test font24 icon-refresh " data-original-title="Update"> </i> </a> <a ><i class="tooltip-test font24 icon-remove-circle" data-original-title="Remove"> </i></a></td>
-                <td class="price">$124.38</td>
-                <td class="total">$120.46</td>
-              </tr>
-            </table>
-          </div>
+      
+            <div class="cart-info">
+        <table class="table table-striped table-bordered">
+        <form method="post" action="cart_update.php">
+          <tr>
+            <th class="image">Image</th>
+            <th class="name">Product Name</th>
+            <th class="model">Model</th>
+            <th class="quantity">Qty</th>
+              <th class="total">Action</th>
+            <th class="price">Unit Price</th>
+            <th class="total">Total</th>
+           
+          </tr>
+          
+          <tr>
+          <div class="cart-view-table-back">
+<form method="post" action="cart_update.php">
+  <tbody>
+    <?php
+     //include_once("config.php"); //include config file
+    if(isset($_SESSION["cart_products"])) //check session var
+    {
+        $total = 0; //set initial total value
+        $b = 0; //var for zebra stripe table 
+        foreach ($_SESSION["cart_products"] as $cart_itm)
+        {
+            //set variables to use in content below
+            $product_name = $cart_itm["product_name"];
+            $product_qty = $cart_itm["product_qty"];
+            $price = $cart_itm["price"];
+            $product_code = $cart_itm["product_code"];
+            $image_url = $cart_itm["image_url"];
+            $subtotal = ($price * $product_qty); //calculate Price x Qty
+            
+           echo '<td class="image"><a href="#"><img title="product" alt="product" src="'.$image_url.'" height="50" width="50"></a></td>';
+            echo '<td class ="name">'.$product_name.'</td>';
+            echo '<td class="description">'.$product_desc.'</td>';
+            echo '<td class="quantity"><input type="text" size="2" maxlength="2" name="product_qty['.$product_code.']" value="'.$product_qty.'" class="col-lg-3 col-md-3 col-xs-6 col-sm-3"></td>';
+            echo '<td class="total"> <a href="#" class="mr10"> <i class="tooltip-test font24 icon-refresh " data-original-title="Update"> </i> </a> 
+               <a href="#"><i class="tooltip-test font24 icon-remove-circle" data-original-title="Remove"> </i></a></td>';
+            echo '<td class="price">'.$currency.$price.'</td>';
+            echo '<td class="total">'.$currency.$subtotal.'</td>';
+            echo '<td><input type="checkbox" name="remove_code[]" value="'.$product_code.'" /></td>';
+            echo '</tr>';
+            $total = ($total + $subtotal); //add subtotal to total var
+        }
+   $grand_total = $total + $shipping_cost; //grand total including shipping cost
+        foreach($taxes as $key => $value){ //list and calculate all taxes in array
+                $tax_amount     = round($total * ($value / 100));
+                $tax_item[$key] = $tax_amount;
+                $grand_total    = $grand_total + $tax_amount;  //add tax val to grand total
+        }
+        
+        $list_tax = '';
+        foreach($tax_item as $key => $value){ //List all taxes
+            $list_tax .= $key. ' : '. $currency. sprintf("%01.2f", $value).'<br />';
+        }
+        $shipping_cost = ($shipping_cost)?'Shipping Cost : '.$currency. sprintf("%01.2f", $shipping_cost).'<br />':'';
+    }
+    ?>
+  </tbody>
+</table>
+<input type="hidden" name="return_url" value="<?php 
+$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+echo $current_url; ?>" />
+</form>
+</div>
+
           <div class="pull-right">
             <table class="table table-striped table-bordered ">
-              <tbody>
-                <tr>
-                  <td><span class="extra bold">Sub-Total :</span></td>
-                  <td><span class="bold">$101.00</span></td>
-                </tr>
-                <tr>
-                  <td><span class="extra bold">Sales Tax (6.00%) :</span></td>
-                  <td><span class="bold">$11.00</span></td>
-                </tr>
-                <tr>
-                  <td><span class="extra bold totalamout">Total :</span></td>
-                  <td><span class="bold totalamout">$120.68</span></td>
-                </tr>
-              </tbody>
+                <tbody>
+                    <tr>
+                        <td class="textright"><b>Sub-Total:</b></td>
+                        <?php echo'<td><span class="bold">'.$subtotal.'</span></td>'?>
+                    </tr>
+                    <tr>
+                        <td class="textright"><b>Eco Tax (-2.00):</b></td>
+                            <?php echo '<td><span class="bold">'.$tax.'</span></td>'?>
+                        </tr>
+                    <tr>
+                        <td class="textright"><b>VAT (17.5%):</b></td>
+                        <td class="textright">$87.50</td>
+                    </tr>
+                    <tr>
+                    <td class="textright"><b>Total:</b></td>
+                        <?php echo  '<td><span class="bold totalamout">'.$grand_total.'</span></td>'?>
+                    </tr>
+                </tbody>
             </table>
             <form action="#">
             <input  type="submit" class="btn btn-orange pull-right" value="CheckOut">
             </form>
-            <form action="catalog.php?page=1">
+            <form action="catalog.php">
             <input type="submit" class="btn btn-orange pull-right mr10" value="Continue Shopping">
             </form>
           </div>
@@ -136,7 +202,7 @@ include_once('header.php');
                     <ul>
                         <li><a href="home.php">Home</a> </li>
                         <li><a href="client.php">My Account</a> </li>
-                        <li><a href="catalog.php?page=1">Shop</a> </li>
+                        <li><a href="catalog.php">Shop</a> </li>
                         <li><a href="contact.php">Contact Us</a> </li>
                         <li><a href="about.php">About</a></li>
                         <li><a href="cart.php">My Cart</a> </li>
@@ -214,16 +280,5 @@ include_once('header.php');
 <script type="text/javascript" src="js/jquery.gmap.js"></script>
 <script type="text/javascript" src="js/jquery.countdown.js"></script>
 <script defer src="js/custom.js"></script>
-
-<!--Google Analytics-->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-69797602-1', 'auto');
-  ga('send', 'pageview');
-</script>
-
 </body>
 </html>
