68b97186c0908d832843840edf4fe3d84db81ce0
Alicia White <alicia.w93.aw@gmail.com>
2015-11-14 19:16:42 -0500

    categorizing (again cause it didn't save the first time)

--- catalog.php
+++ catalog.php
@@ -53,20 +53,12 @@ $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_
           <div class="sidewidt">
             <h1 class="heading1"><span class="maintext"><i class="icon-th-list"></i> Categories</span></h1>
             <ul class="nav nav-list categories">
-              <li> <a href="category.php">Furniture</a>
-                <ul>
-                  <li> <a href="catalog.php?page=1">Kitchen </a> </li>
-                  <li> <a href="catalog.php?page=1">Outdoor </a> </li>
-                  <li> <a href="catalog.php?page=1">Living Room</a> </li>
-                  <li> <a href="catalog.php?page=1">Bed &amp; Bath</a> </li>
-                </ul>
-              </li>
-              <li> <a href="catalog.php?page=1">Kitchen</a> </li>
-              <li> <a href="catalog.php?page=1">Electronics </a> </li>
-              <li> <a href="catalog.php?page=1">Bathroom </a> </li>
-              <li> <a href="catalog.php?page=1">Outdoor</a> </li>
-              <li> <a href="catalog.php?page=1">Living Room</a> </li>
-              <li> <a href="catalog.php?page=1">Miscellanious</a> </li>
+              <li> <a href="#" id="navFurn" class="categoryBtnFurniture">Furniture</a></li>
+              <li> <a href="#" id="navKit" class="categoryBtnKitchen">Kitchen</a> </li>
+              <li> <a href="#" id="navElect" class="categoryBtnElectronics">Electronics </a> </li>
+              <li> <a href="#" id="navBath" class="categoryBtnBathroom">Bathroom </a> </li>
+              <li> <a href="#" id="navBed" class="categoryBtnBedroom">Bedroom</a> </li>
+              <li> <a href="#" id="navLiv" class="categoryBtnLivingroom">Living Room</a> </li>
             </ul>
           </div>
           <!--  Hottest New Products -->
@@ -81,7 +73,7 @@ $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_
         			//$myNew = mysql_query($new,$con);  
               while(($row = $myNew->fetch_object()) && ($zero<=$limit)){
 	              $zero++;
-	              echo "<li> <img width='50' height='50' src=" .$row->image_url. " alt='product' title='product'> <a class='productname' href='product.php?id=".$row->product_id."'>" .$row->product_name. "</a><span class='procategory'>Furniture</span> <span class='price'>" . $row->price. "</span> </li>";
+	              echo "<li> <img width='50' height='50' src=" .$row->image_url. " alt='product' title='product'> <a class='productname' href='product.php?id=".$row->product_id."'>" .$row->product_name. "</a><span class='procategory'>".$row->category."</span> <span class='price'>" . $row->price. "</span> </li>";
               }
             ?>
             </ul>
@@ -192,26 +184,26 @@ if($view==24){
 				$zero=1;
                         while(($row = $myAll->fetch_object())&&($zero<=$view)){
 							$zero++;
-				  ?>
-                    <li>
-                      <div class="thumbnail">
-                        <div class="row">
-						<?php echo "<div class='col-lg-4 col-md-4 col-xs-12 col-sm-6 span3'><a href='product.php?id=".$row->product_id."'><img alt='' src=" . $row->image_url. "></a> </div>";
-                              echo "<div class='col-lg-6 col-md-6 col-xs-12 col-sm-12'><a class='prdocutname' href='product.php?id=".$row->product_id."'>" .$row->product_name. "</a>";
-                              echo "<div class='productdiscrption'>" .$row->description. "<br></div>";
-                              echo "<div class='price'>";
-                              echo "<div class='pricenew'>" .$row->price. "</div>";
-                              echo "<div class='ratingstar'>";
-                              echo "<div class='rw-ui-container' data-urid=" . $row->product_id. "></div>"; ?>
-                              		</div>
-                                    <a  class='btn btn-orange btn-large addtocartbutton pull-left'>Add to Cart</a>
-                            	</div>					
-                            </div>
-                            
-                          </div>
-                         
+				  
+                echo "<li>";
+                  echo "<div class='thumbnail ".$row->category."' >";
+                    echo "<div class='row'>";
+  			                  echo "<div class='col-lg-4 col-md-4 col-xs-12 col-sm-6 span3'><a href='product.php?id=".$row->product_id."'><img alt='' src=" . $row->image_url. "></a> </div>";
+                          echo "<div class='col-lg-6 col-md-6 col-xs-12 col-sm-12'><a class='prdocutname' href='product.php?id=".$row->product_id."'>" .$row->product_name. "</a>";
+                          echo "<div class='productdiscrption'>" .$row->description. "<br></div>";
+                          echo "<div class='price'>";
+                          echo "<div class='pricenew'>" .$row->price. "</div>";
+                          echo "<div class='ratingstar'>";
+                          echo "<div class='rw-ui-container' data-urid=" . $row->product_id. "></div>";?>
+                          		</div>
+                                <a  class='btn btn-orange btn-large addtocartbutton pull-left'>Add to Cart</a>
+                        	</div>					
                         </div>
-                    </li>
+                        
+                      </div>
+                     
+                    </div>
+                </li>
                    <?php } }
 elseif($view==36){
 				$zero=1;
@@ -314,169 +306,72 @@ else{
     </div>
   </section>
 </div>
-
-<footer id="footer">
-    <section class="footersocial">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 span3 info">
-                    <h2> <i class="icon-link"></i> SiteMap </h2>
-                    <ul>
-                        <li><a href="home.php">Home</a> </li>
-                        <li><a href="client.php">My Account</a> </li>
-                        <li><a href="catalog.php?page=1">Shop</a> </li>
-                        <li><a href="contact.php">Contact Us</a> </li>
-                        <li><a href="about.php">About</a></li>
-                        <li><a href="cart.php">My Cart</a> </li>
-                        <li><a href="register.php">Sign-Up</a> </li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 span3 contact">
-                    <h2> <i class="icon-phone-sign"></i> Contact Info </h2>
-                    <ul>
-                        <li class="location"> 404 Not found Rd.‎ Melbourne, Fl 32903</li>
-                        <li class="phone">(800)555-7890 &nbsp; (877)555-7890</li>
-                        <li class="mobile"> #Bachpad</li>
-                        <li class="email"> sales@batchpad.com</li>
-                    </ul>
-                </div>
-                <!-- Testimonial-->
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-12 span3">
-                    <div class="sidewidt">
-                        <h2 class="heading2"><span><i class="icon-edit"></i> Testimonials</span></h2>
-                        <div class="flexslider" id="testimonialsidebar">
-                            <ul class="slides">
-                                <li>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."<br>"Lorem Ipsum is simply dummy text of the printing and industry.<br><br>
-                                    <span class="pull-left orange">By : Lorem Ipsum</span> 
-                                </li>
-                                <li>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."<br>"Lorem Ipsum is simply dummy text of the printing and industry.<br><br>
-                                    <span class="pull-left orange">By : Lorem Ipsum</span> 
-                                </li>
-                                <li>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."<br>"Lorem Ipsum is simply dummy text of the printing and industry.<br><br>
-                                    <span class="pull-left orange">By : Lorem Ipsum</span> 
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-6 span3 facebook">
-                    <h2> <i class="icon-facebook-sign"></i> Facebook </h2>
-                    <div class="seperator"></div>
-                    <div class="seperator1"></div>
-                    <div id="fb-root"></div>
-                    <div class="fb-like-box" data-href="https://www.facebook.com/BachelorHaus" data-width="292" data-show-faces="true" data-colorscheme="dark" data-stream="false" data-show-border="false" data-header="false"  data-height="240"></div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="copyrightbottom">
-        <div class="container">
-            <div class="row">
-            <div class="col-lg-3 col-md-3 col-xs-12 col-sm-12 span5 col-lg-3 col-md-3 col-xs-12 col-sm-12 paymentsicons"></div>
-                <div class="col-lg-6 col-md-6 col-xs-12 col-sm-12 textcenter"> This site is not official and is an assignment for a UCF Digital Media course - Designed by Justin Duncan </div>
-                <div class="col-lg-3 col-md-3 col-xs-12 col-sm-12 span5 col-lg-3 col-md-3 col-xs-12 col-sm-12 paymentsicons"> <img title="PayPal" alt="paypal" src="img/payment_paypal.png"> <img title="American Express" alt="american-express" src="img/payment_american.png"><img title="Maestro" alt="maestro" src="img/payment_maestro.png"> <img title="Discover" alt="discover" src="img/payment_discover.png"> </div>
-            </div>
-        </div>
-    </section>
-    <a id="gotop" href="#">Back to top</a>
-</footer>
-<!-- javascript
-    ================================================== --> 
-<!-- Placed at the end of the document so the pages load faster --> 
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-<script src="js/jquery.js"></script>
-<script src="js/jquery-migrate.min.js"></script> 
-<script type="text/javascript" src="js/jquery.easing.js"></script> 
-<script src="js/respond.min.js"></script> 
-<script src="js/bootstrap.min.js"></script> 
-<script  src="js/jquery.prettyPhoto.js"></script> 
-<script defer src="js/jquery.flexslider.js"></script> 
-<script src="layerslider/js/greensock.js" type="text/javascript"></script> 
-<script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script> 
-<script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script> 
-<script type="text/javascript" src="js/jquery.tweet.js"></script> 
-<script  src="js/zoomsl-3.0.min.js"></script> <script  type="text/javascript" src="js/jquery.validate.js"></script> 
-<script type="text/javascript"  src="js/jquery.carouFredSel-6.1.0-packed.js"></script> 
-<script type="text/javascript"  src="js/jquery.mousewheel.min.js"></script> 
-<script type="text/javascript"  src="js/jquery.touchSwipe.min.js"></script> 
-<script type="text/javascript" src="js/jquery.gmap.js"></script>
-<script type="text/javascript" src="js/jquery.countdown.js"></script>
-<script defer src="js/custom.js"></script>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<script type="text/javascript">(function(d, t, e, m){
-    
-    // Async Rating-Widget initialization.
-    window.RW_Async_Init = function(){
-                
-        RW.init({
-            huid: "259474",
-            uid: "3b0bc03895785eed657c9e205359ca03",
-            source: "website",
-            options: {
-                "advanced": {
-                    "font": {
-                        "hover": {
-                            "color": "#B7D086"
-                        },
-                        "size": "12px",
-                        "bold": true,
-                        "color": "#B7D086"
-                    }
-                },
-                "size": "tiny",
-                "label": {
-                    "background": "#427E53"
-                },
-                "style": "oxygen_green",
-                "isDummy": false
-            } 
-        });
-        RW.render();
-    };
-        // Append Rating-Widget JavaScript library.
-    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
-        l = d.location, ck = "Y" + t.getFullYear() + 
-        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
-        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
-        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
-    if (d.getElementById(id)) return;              
-    rw = d.createElement(e);
-    rw.id = id; rw.async = true; rw.type = "text/javascript";
-    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
-    s.parentNode.insertBefore(rw, s);
-    }(document, new Date(), "script", "rating-widget.com/"));
-    
-	
-	function change(){
-    	document.getElementById("view_form").submit();
-		
-	}
-
-	function change2(){
-    	document.getElementById("sort_form").submit();
-		
-	}
-    
-    </script>
-
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-69797602-1', 'auto');
-  ga('send', 'pageview');
+$(document).ready(function() {
+  
+  $(".categoryBtnFurniture").on("click", function(){
+    hideProducts();
+    $(".furniture").show();
+  });
+
+  $(".categoryBtnKitchen").on("click", function(){
+    hideProducts();
+    $(".kitchen").show();
+  });
+
+  $(".categoryBtnElectronics").on("click", function(){
+    hideProducts();
+    $(".electronic").show();
+  });
+
+  $(".categoryBtnBathroom").on("click", function(){
+    hideProducts();
+    $(".bathroom").show();
+  });
+
+  $(".categoryBtnBedroom").on("click", function(){
+    hideProducts();
+    $(".bedroom").show();
+  });
+
+  $(".categoryBtnLivingroom").on("click", function(){
+    hideProducts();
+    $(".livingroom").show();
+  });
+
+  $("#navFurn").on("click", function(){
+    hideProducts();
+    $(".furniture").show();
+  });
+
+  $("#navKit").on("click", function(){
+    hideProducts();
+    $(".kitchen").show();
+  });
+
+  $("#navElect").on("click", function(){
+    hideProducts();
+    $(".electronic").show();
+  });
+
+  $("#navBath").on("click", function(){
+    hideProducts();
+    $(".bathroom").show();
+  });
+
+  $("#navBed").on("click", function(){
+    hideProducts();
+    $(".bedroom").show();
+  });
+
+  $("#navLiv").on("click", function(){
+    hideProducts();
+    $(".livingroom").show();
+  });
+
+});
 </script>
 
 <?php
-mysql_close($con);
+include('footer.php');
 ?>
-</body>
-</html>
