89e3a5da76ea836c04e0d87b4b8dbb4824daab16
justinduncan2010 <justinduncan2010@gmail.com>
2015-11-15 12:22:09 -0500

    added both connection queries

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,8 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'Jade7369!', 'ju655443');  
+//local
+$mysqli = new mysqli('localhost', 'root', 'Jade7369!', 'ju655443');
+//sulley
+//$mysqli = new mysqli('sulley.cah.ucf.edu', 'ju655443', 'Jade7369!', 'ju655443');  
 
 // Check connection
 if (mysqli_connect_errno())
