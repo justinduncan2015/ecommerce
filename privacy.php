1b255d59a42e86ada9e887accdf19ce9f082070f
rockenbok <devin@iditall.com>
2015-11-17 12:05:58 -0500

    removed shopify section
    removed shopify section

--- privacy.php
+++ privacy.php
@@ -67,19 +67,9 @@ Batchpad.com
                     We may disclose your personal information if we are required by law to do so or if you violate our Terms of Service. 
 
                 </div>
+                
                 <div>
-                    <h2 class="heading2 icons">SECTION 4 - SHOPIFY</h2>
-                    Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you. 
-Your data is stored through Shopify’s data storage, databases and the general Shopify application. They store your data on a secure server behind a firewall.
-
-Payment:
-If you choose a direct payment gateway to complete your purchase, then Shopify stores your credit card data. It is encrypted through the Payment Card Industry Data Security Standard (PCI-DSS). Your purchase transaction data is stored only as long as is necessary to complete your purchase transaction. After that is complete, your purchase transaction information is deleted.
-All direct payment gateways adhere to the standards set by PCI-DSS as managed by the PCI Security Standards Council, which is a joint effort of brands like Visa, MasterCard, American Express and Discover. 
-PCI-DSS requirements help ensure the secure handling of credit card information by our store and its service providers.
-For more insight, you may also want to read Shopify’s Terms of Service here or Privacy Statement here.
-                </div>
-                <div>
-                    <h2 class="heading2 icons">SECTION 5 - THIRD-PARTY SERVICES</h2>
+                    <h2 class="heading2 icons">SECTION 4 - THIRD-PARTY SERVICES</h2>
                     In general, the third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to us. 
 However, certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions.
 For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers. 
@@ -91,13 +81,13 @@ Links
 When you click on links on our store, they may direct you away from our site. We are not responsible for the privacy practices of other sites and encourage you to read their privacy statements.
                 </div>
                 <div>
-                    <h2 class="heading2 icons">SECTION 6 - SECURITY</h2>
+                    <h2 class="heading2 icons">SECTION 5 - SECURITY</h2>
                     To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.
 If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption. Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards.
 
                 </div>
                 <div>
-                    <h2 class="heading2 icons">SECTION 7 - COOKIES</h2>
+                    <h2 class="heading2 icons">SECTION 6 - COOKIES</h2>
                     Here is a list of cookies that we use. We’ve listed them here so you that you can choose if you want to opt-out of cookies or not.
 _session_id, unique token, sessional, Allows Shopify to store information about your session (referrer, landing page, etc).
 _shopify_visit, no data held, Persistent for 30 minutes from the last visit, Used by our website provider’s internal stats tracker to record the number of visits
@@ -108,13 +98,13 @@ storefront_digest, unique token, indefinite If the shop has a password, this is 
 
                 </div>
                 <div>
-                    <h2 class="heading2 icons">SECTION 8 - AGE OF CONSENT
+                    <h2 class="heading2 icons">SECTION 7 - AGE OF CONSENT
 </h2>
                     By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.
 
                 </div>
                 <div>
-                    <h2 class="heading2 icons">SECTION 9 - CHANGES TO THIS PRIVACY POLICY
+                    <h2 class="heading2 icons">SECTION 8 - CHANGES TO THIS PRIVACY POLICY
 </h2>
                     We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.
 If our store is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you.
